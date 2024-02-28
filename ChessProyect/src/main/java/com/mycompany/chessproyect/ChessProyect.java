439c3b0eadeb36535441842726348060532844ae
Cisc0Matic <agusbarriobarrio87@gmail.com>
2024-02-28 10:00:01 -0300

    1

---
+++ ChessProyect/src/main/java/com/mycompany/chessproyect/ChessProyect.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chessproyect;
+import java.util.Scanner;
+
+public class ChessProyect {
+    public static void main(String[] args) {
+    }
+
+}
